c26f26c39fe99b14b3024d18db9e5b964d07d055
huangchaolin <huangchaolin@xylink.com>
2018-12-05 17:53:50 +0800

    feat: use typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component<any, any> {
   render() {
     return (
       <div className="App">
