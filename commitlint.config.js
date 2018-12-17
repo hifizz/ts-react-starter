5ab1863c5863a8b308a975ceb3c4d812646e896e
hifizz <276161014@qq.com>
2018-12-17 23:09:08 +0800

    chore: use commitlint for validate commitizen

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  // rules & wildcards copy from https://github.com/erikmueller/conventional-changelog-lint-config-atom/blob/master/index.js
+  parserPreset: "conventional-changelog-atom",
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        /** WIP means Working in progress，used in GitLab usually  */
+        "WIP",
+        /** Merge branch */
+        "merge",
+        "build",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test"
+      ]
+    ]
+  }
+};
